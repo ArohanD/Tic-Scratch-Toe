1a2e0adcf9aa9a11bbf65b50f4e9c8a61c8d63e9
Arohan Dutt <arohandutt@gmail.com>
2019-08-12 23:13:37 -0500

    update to server methods

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static('../public'));
+app.use('/', express.static('./Public'));
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', './Public/index.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => console.log('Tic Scratch Toe is ready on port 3000!'))
