ac35df9be4d139a5ccf523294f79a404ba525b68
Arohan Dutt <arohandutt@gmail.com>
2019-08-12 23:06:30 -0500

    Modify App.js for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use('/', express.static('../public'));
 
 app.listen(process.env.PORT || 3000, () => console.log('Tic Scratch Toe is ready on port 3000!'))
